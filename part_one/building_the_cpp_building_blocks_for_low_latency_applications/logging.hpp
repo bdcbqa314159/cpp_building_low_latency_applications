c55d435e096d2c8e0bf09bfb4e189f0a65ed0f57
Bernardo David COHEN BERAUN <79301157+bdcbqa314159@users.noreply.github.com>
2025-03-09 13:51:33 +0000

    part one - building the c++ building blocks for low-latency applications

--- part_one/building_the_cpp_building_blocks_for_low_latency_applications/logging.hpp
+++ part_one/building_the_cpp_building_blocks_for_low_latency_applications/logging.hpp
@@ -6,8 +6,8 @@
 
 #include "macros.hpp"
 #include "lf_queue.hpp"
-#include "time_utils.hpp"
 #include "thread_utils.hpp"
+#include "time_utils.hpp"
 
 namespace Common
 {
@@ -45,19 +45,12 @@ namespace Common
 
     class Logger final
     {
-    private:
-        const std::string file_name_;
-        std::ofstream file_;
-
-        LFQueue<LogElement> queue_;
-        std::atomic<bool> running_ = {true};
-        std::thread *logger_thread_ = nullptr;
-
     public:
         auto flushQueue() noexcept
         {
             while (running_)
             {
+
                 for (auto next = queue_.getNextToRead(); queue_.size() && next; next = queue_.getNextToRead())
                 {
                     switch (next->type_)
@@ -93,35 +86,37 @@ namespace Common
                     queue_.updateReadIndex();
                 }
                 file_.flush();
+
                 using namespace std::literals::chrono_literals;
                 std::this_thread::sleep_for(10ms);
             }
         }
 
-        explicit Logger(const std::string &file_name) : file_name_(file_name), queue_(LOG_QUEUE_SIZE)
+        explicit Logger(const std::string &file_name)
+            : file_name_(file_name), queue_(LOG_QUEUE_SIZE)
         {
             file_.open(file_name);
-            ASSERT(file_.is_open(), "Could not open log file: " + file_name_);
+            ASSERT(file_.is_open(), "Could not open log file:" + file_name);
             logger_thread_ = createAndStartThread(-1, "Common/Logger " + file_name_, [this]()
                                                   { flushQueue(); });
-            ASSERT(logger_thread_ != nullptr, "Failed to start Logger thread");
+            ASSERT(logger_thread_ != nullptr, "Failed to start Logger thread.");
         }
 
         ~Logger()
         {
             std::string time_str;
-            std::cerr << Common::getCurrentTimeStr(&time_str) << " flushing and closing Logger for file: " << file_name_ << "\n";
+            std::cerr << Common::getCurrentTimeStr(&time_str) << " Flushing and closing Logger for " << file_name_ << std::endl;
 
             while (queue_.size())
             {
                 using namespace std::literals::chrono_literals;
                 std::this_thread::sleep_for(1s);
             }
-
             running_ = false;
             logger_thread_->join();
+
             file_.close();
-            std::cerr << Common::getCurrentTimeStr(&time_str) << " flushing and closing Logger for file: " << file_name_ << " exiting...\n";
+            std::cerr << Common::getCurrentTimeStr(&time_str) << " Logger for " << file_name_ << " exiting." << std::endl;
         }
 
         auto pushValue(const LogElement &log_element) noexcept
@@ -197,11 +192,13 @@ namespace Common
                 if (*s == '%')
                 {
                     if (UNLIKELY(*(s + 1) == '%'))
+                    { // to allow %% -> % escape character.
                         ++s;
+                    }
                     else
                     {
-                        pushValue(value);
-                        log(s + 1, args...);
+                        pushValue(value);    // substitute % with the value specified in the arguments.
+                        log(s + 1, args...); // pop an argument and call self recursively.
                         return;
                     }
                 }
@@ -210,6 +207,7 @@ namespace Common
             FATAL("extra arguments provided to log()");
         }
 
+        // note that this is overloading not specialization. gcc does not allow inline specializations.
         auto log(const char *s) noexcept
         {
             while (*s)
@@ -217,20 +215,35 @@ namespace Common
                 if (*s == '%')
                 {
                     if (UNLIKELY(*(s + 1) == '%'))
+                    { // to allow %% -> % escape character.
                         ++s;
+                    }
                     else
                     {
-                        FATAL("missing arguments for log()");
+                        FATAL("missing arguments to log()");
                     }
                 }
                 pushValue(*s++);
             }
         }
 
+        // Deleted default, copy & move constructors and assignment-operators.
         Logger() = delete;
+
         Logger(const Logger &) = delete;
+
         Logger(const Logger &&) = delete;
+
         Logger &operator=(const Logger &) = delete;
+
         Logger &operator=(const Logger &&) = delete;
+
+    private:
+        const std::string file_name_;
+        std::ofstream file_;
+
+        LFQueue<LogElement> queue_;
+        std::atomic<bool> running_ = {true};
+        std::thread *logger_thread_ = nullptr;
     };
 }
